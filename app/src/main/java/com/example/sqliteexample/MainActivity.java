625b0fb5330325ee24d4e2ff6d72d9ac1d7dbbe7
BabyBear <suriyon.saramul@yahoo.com>
2019-02-03 16:44:57 +0700

    first commit: add comment to MainActivity

---
+++ app/src/main/java/com/example/sqliteexample/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.sqliteexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ContactSQLiteHelper helper;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get all contacts
+        
+        helper = new ContactSQLiteHelper(this);
+        List<Contact> contacts = helper.selectAll();
+
+        initComponent();
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new ContactAdapter(contacts);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initComponent() {
+        recyclerView = findViewById(R.id.recycleview);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.add_contact){
+            Intent intent = new Intent(this, AddContact.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
